9db77cbc3d5388504a665ba1a23a7b8dc8bbf87e
secondsecurity323 <bottelegram323@gmail.com>
2025-06-06 11:59:00 -0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 import telebot
 from flask import Flask, request
 
-TOKEN = '7334013777:AAFSZ7ITYkxV-MR0z9NKWVcTxd9WzvA0z5Y'
+TOKEN = '7334013777:AAEQVcRi6MpiDok7_Tsl9JQSjPWYAxHSH-g'
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
@@ -15,12 +15,18 @@ def webhook():
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, "recived")
+    # ساخت متن با نام یا آیدی کاربر و متن پیام
+    user_info = message.from_user.username or message.from_user.first_name or "ناشناس"
+    text_to_send = f"پیام از کاربر {user_info}:\n{message.text}"
+    
+    # ارسال پیام به همان چت (صفحه ربات)
+    bot.send_message(message.chat.id, text_to_send)
+    
+    # پاسخ دادن به کاربر
+    bot.reply_to(message, "received")
 
 if __name__ == '__main__':
     bot.remove_webhook()
     bot.set_webhook(url=f'https://my-telegram-bot-wi2v.onrender.com/{TOKEN}')
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-
